606418dcc3fb94b6e57969ae296b0bd5574b4320
Sigve Hansen <sighan12@gmail.com>
2022-02-28 23:35:05 +0100

    feat: implement basic functionality

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,21 +1,26 @@
-<script>
-import { defineComponent } from 'vue';
+<script setup>
+import { onBeforeMount, ref } from 'vue';
 import ProductViewer from '@/product-viewer.vue';
 
-export default defineComponent({
-  name: 'ServeDev',
-  components: {
-    ProductViewer
-  },
-  methods: {
-    onDragRight(){
-      console.log('right')
-    },
-    onDragLeft(){
-      console.log('left')
-    },
+const directionRef = ref()
+const frameRef = ref()
+const images = ref([])
+
+function onDragRight({ currentFrame, totalFrames, direction }){
+  frameRef.value.innerText = currentFrame + ' / ' + totalFrames
+  directionRef.value.innerText = direction
+}
+
+function onDragLeft({ currentFrame, totalFrames, direction }){
+  frameRef.value.innerText = currentFrame + ' / ' + totalFrames
+  directionRef.value.innerText = direction
+}
+
+onBeforeMount(() => {
+  for(let i = 1; i <= 34; i++) {
+    images.value.push(`https://spritespin.ginie.eu/images/rad_zoom_${i<10?'00':'0'}${i}.jpg`)
   }
-});
+})
 </script>
 
 <template>
@@ -24,26 +29,47 @@ export default defineComponent({
       <product-viewer 
         @drag-right="onDragRight"
         @drag-left="onDragLeft"
+        :images="images"
+        :speed="10"
       >
-        <template #header>Header</template>
+        <template #header="{ activeFrame }">
+          <div class="product-title">
+            <b>From scoped slot</b>
+            <div>Active frame: <span>{{ activeFrame }}</span></div>
+          </div>
+        </template>
       </product-viewer>
+      <b>From events:</b>
+      <div>Active frame: <span ref="frameRef"></span></div>
+      <div>Delta: <span ref="directionRef"></span></div>
     </div>
   </div>
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 body {
   margin: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
+
 #app {
+  padding: 2rem;
   margin: 0;
   height: 50vh;
   width: 100vw;
 }
 
 .wrapper {
-  width: 400px;
-  height: 400px;
+  width: 480px;
+  height: 327px;
   margin: 0 auto;
+}
+
+.product-title {
+  padding: 0.75rem 1rem;
 }
 </style>
--- src/product-viewer.vue
+++ src/product-viewer.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { ref, defineEmits } from 'vue'
+import { ref, defineEmits, onMounted, onUnmounted } from 'vue'
 
 const emit = defineEmits(['drag-right','drag-left'])
+const props = defineProps({
+  images: Array,
+  speed: Number
+})
 
-// drag handling
+const activeFrame = ref(1)
 const isDragging = ref(false)
 
 function setIsDragging(state) {
@@ -14,31 +18,63 @@ function onDrag(event) {
   if(!isDragging.value) return
   
   if(event.movementX > 0) {
-    emit('drag-right', event.movementX)
+    activeFrame.value --
+    if(activeFrame.value < 1) activeFrame.value = props.images.length
+
+    emit('drag-right', {
+      currentFrame: activeFrame.value,
+      totalFrames: props.images.length,
+      direction: event.movementX
+    })
   }
 
   if(event.movementX < 0) {
-    emit('drag-left', event.movementX)
+    activeFrame.value ++
+    if(activeFrame.value > props.images.length) activeFrame.value = 1
+
+    emit('drag-left', {
+      currentFrame: activeFrame.value,
+      totalFrames: props.images.length,
+      direction: event.movementX
+    })
   }
 }
+
+onMounted(() => {
+  document.addEventListener('mousemove', onDrag)
+  document.addEventListener('mouseup', () => {
+    if(!isDragging.value) return
+    setIsDragging(false)
+  })
+})
+
+onUnmounted(() => {
+  document.removeEventListener('mousemove', onDrag)
+  document.removeEventListener('mouseup', () => {
+    if(!isDragging.value) return
+    setIsDragging(false)
+  })
+})
 </script>
 
 <template>
   <div
     class="product-viewer"
     @mousedown="setIsDragging(true)"
-    @mouseup="setIsDragging(false)"
-    @mousemove="onDrag"
   >
     <header class="product-viewer__header" v-if="$slots.header">
-      <slot name="header" />
+      <slot name="header" :active-frame="activeFrame" />
     </header>
-    <canvas
-      ref="productViewer"
-      class="product-viewer__canvas"
-    >
-      <slot name="js-disabled">This component does not function properly without javascript</slot>
-    </canvas>
+    <div class="product-viewer__images">
+      <img
+        class="product-viewer__image"
+        v-for="(image, i) in images"
+        :key="`product-viewer-image-${i+1}`"
+        :src="image"
+        alt=""
+        :class="{ 'product-viewer__image--active': activeFrame == i+1 }"
+      />
+    </div>
     <footer class="product-viewer__footer" v-if="$slots.footer">
       <slot name="footer" />
     </footer>
@@ -51,11 +87,25 @@ function onDrag(event) {
   height: 100%;
   width: 100%;
   background: #eaeaea;
+  font: inherit;
 }
 
-.product-viewer__canvas {
+.product-viewer__images {
   height: 100%;
   width: 100%;
+}
+
+.product-viewer__image {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+  display: none;
+  pointer-events: none;
+  user-select: none;
+}
+
+.product-viewer__image--active {
+  display: block;
 }
 
 .product-viewer__header,
